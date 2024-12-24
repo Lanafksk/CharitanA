c001f74afb95b7fc58cd6d2ea310c4a5fab525b9
Kenny Nguyen <s3927238@rmit.edu.vn>
2024-12-24 23:06:22 +0700

    Updated Project related API

--- Backend/src/modules/encryption/models/authKeyModel.js
+++ Backend/src/modules/encryption/models/authKeyModel.js
@@ -7,7 +7,7 @@ dotenv.config();
 const clusterURI = process.env.MONGO_URI;
 
 // Connect to respective Database
-const db = connectDB('key', clusterURI);
+const db = connectDB('charitan', clusterURI);
 
 // Define the AuthKey Schema
 const authKeySchema = new mongoose.Schema(
