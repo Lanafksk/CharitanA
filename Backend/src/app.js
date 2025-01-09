7beaa08dac50d4bddfc740c54c6a1e0afbe3ace1
Kenny Nguyen <s3927238@rmit.edu.vn>
2025-01-09 19:24:23 +0700

    Implemented image upload

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const routes = require('./routes/index'); // Centralized routes
+const redis = require('redis');
 
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || 'localhost';
@@ -10,10 +11,15 @@ const HOST = process.env.HOST || 'localhost';
 dotenv.config();
 
 const app = express();
+const redisClient = redis.createClient({
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT
+});
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.get('/', (req, res) => {
