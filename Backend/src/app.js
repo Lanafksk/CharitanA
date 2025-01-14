693096399bd9c4961643264442cce676978ce144
MinT <tnm1042002@gmail.com>
2025-01-14 07:48:27 +0700

    update ipv4 for app.js

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,8 @@ const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const cors = require('cors');
 const routes = require('./routes/index'); // Centralized routes
+const EXTERNAL_HOST = process.env.EXTERNAL_HOST || HOST; // Use EXTERNAL_HOST if defined, otherwise fall back to HOST
+
 
 const PORT = process.env.PORT || 4000;
 const HOST = process.env.HOST || 'localhost';
@@ -26,4 +28,5 @@ app.use('/api', routes);
 // Start server
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Sever running in http://${HOST}:${PORT}`);
+    console.log(`Sever running in http://${EXTERNAL_HOST}:${PORT}`);
 });
